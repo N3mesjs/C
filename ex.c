666453a431b627872f700b5417d88279383967fe
N3mesjs <84539974+N3mesjs@users.noreply.github.com>
2026-04-01 14:00:13 +0200

    .

--- ex.c
+++ ex.c
@@ -1,20 +1,12 @@
 #include <stdio.h>
 
-/*
-   PRE: lung>0, lung=|A|
-   POST restituisce l'indirizzo del valore minore in B
-*/
-int * ind_minimo(int B[], int lung) {
-    int * p = B;
-    for(int i=1; i<lung; i+=1) {
-       if(B[i] < *p) 
-           p=&B[i];
-    }
-    return p;
-}
+int main() {
+  int matrix[3][2] = {
+    {1, 2},
+    {3, 4},
+    {5, 6}
+  };
 
-int main () {
-  int A[] = {4,1,2,3};
-  int *p = ind_minimo(A, 4);
-  printf("%d\n", *p);
+  int (*p)[2] = matrix;
+  return 0;
 }
